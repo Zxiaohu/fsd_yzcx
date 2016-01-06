0e03fe2b2357cf445edfb7e93b4138db5357d3d7
zxiaohu <747757998@qq.com>
2016-01-06 23:56:50 +0800

    添加请求网络时的进度条

--- FSD物业助手/src/com/fsd/yzcx/tools/http/HttpTools.java
+++ FSD物业助手/src/com/fsd/yzcx/tools/http/HttpTools.java
@@ -1,9 +1,12 @@
 package com.fsd.yzcx.tools.http;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 
+import com.fsd.yzcx.R;
 import com.fsd.yzcx.tools.JsonTools;
 import com.fsd.yzcx.tools.LogUtil;
 import com.fsd.yzcx.tools.SystemTools;
@@ -118,15 +121,21 @@ public class HttpTools {
 			) {
 
 		HttpUtils httpUtils = new HttpUtils(6 * 3000);
-
+		final ProgressDialog progressDialog = new ProgressDialog(MyApplication.getContext());
+		progressDialog.setView(View.inflate(MyApplication.getContext(), R.layout.http_progress, null));
+		
+		//封装框架的请求接口
 		httpUtils.send(HttpMethod.POST, url,params, new RequestCallBack<String>() {
-
+			
 			public void onFailure(HttpException arg0, String arg1) {
+				progressDialog.dismiss();//隐藏加载栏
 				SystemTools.showToastInfo(MyApplication.getContext(), tag+"请求失败", 3000, 2);
 				
 			}
 
 			public void onSuccess(ResponseInfo<String> arg0) {
+				
+				progressDialog.dismiss();//隐藏加载栏
 				//SystemTools.showToastInfo(context, tag+"请求成功", 3000, 2);
 				String response=arg0.result.toString();
 				LogUtil.i(RoomLoginPager.class.getSimpleName(),response);
@@ -142,10 +151,13 @@ public class HttpTools {
 			public void onLoading(long total, long current, boolean isUploading) {
 				super.onLoading(total, current, isUploading);
 			
+				progressDialog.show();//显示加载栏
+				
 			}
 
 			public void onStart() {
 				super.onStart();
+				progressDialog.show();//显示加载栏
 
 			}
 		});
